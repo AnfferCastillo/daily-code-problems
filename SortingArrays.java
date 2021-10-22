1a83e690bf3d0cc4ee34a4fc4800b02016d11d2f
Anffer <anffer.castillo@gmail.com>
2021-10-22 16:10:29 -0300

    merge sort

--- SortingArrays.java
+++ SortingArrays.java
@@ -8,9 +8,9 @@ public class SortingArrays {
 
         // SortingArrays.bubbleSort(array);
         // SortingArrays.selectSort(array);
-        SortingArrays.insertionSort(array);
-        List<Integer> l = Arrays.asList(array);
-        System.out.println(l);
+        // SortingArrays.insertionSort(array);
+        Integer[] l = SortingArrays.mergeSort(array);
+        System.out.println(Arrays.asList(l));
 
     }
 
@@ -59,6 +59,50 @@ public class SortingArrays {
             }
 
             array[j + 1] = currentItem;
+        }
+    }
+
+    public static Integer[] mergeSort(Integer[] array) {
+        if (array.length == 1)
+            return array;
+
+        int mid = array.length / 2;
+
+        Integer[] left = mergeSort(Arrays.copyOfRange(array, 0, mid));
+        Integer[] right = mergeSort(Arrays.copyOfRange(array, mid, array.length));
+
+        array = new Integer[left.length + right.length];
+
+        int sortedIndex = 0, leftIndex = 0, rightIndex = 0;
+
+        while (sortedIndex < array.length && leftIndex < left.length && rightIndex < right.length) {
+
+            if (left[leftIndex] < right[rightIndex]) {
+                array[sortedIndex] = left[leftIndex];
+                leftIndex++;
+            } else if (left[leftIndex] > right[rightIndex]) {
+                array[sortedIndex] = right[rightIndex];
+                rightIndex++;
+            }
+
+            sortedIndex++;
+        }
+
+        if (sortedIndex < array.length) {
+            if (leftIndex < left.length) {
+                addTheRest(array, sortedIndex, left, leftIndex);
+            } else if (rightIndex < right.length) {
+                addTheRest(array, sortedIndex, right, rightIndex);
+            }
+        }
+
+        return array;
+    }
+
+    private static void addTheRest(Integer[] sortedArray, int sortedIndex, Integer[] rest, int index) {
+        for (; index < rest.length; index++) {
+            sortedArray[sortedIndex] = rest[index];
+            sortedIndex++;
         }
     }
 
