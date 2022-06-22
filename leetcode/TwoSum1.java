8778f6ec3ab63e3ae4fc09e6885eaf6dd2710492
Anffer Castillo <anffer.castillo@gmail.com>
2022-06-21 21:29:40 -0300

    Two Sum 1

---
+++ leetcode/TwoSum1.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such
+ * that they add up to target.
+ *
+ * <p>You may assume that each input would have exactly one solution, and you may not use the same
+ * element twice.
+ *
+ * <p>You can return the answer in any order.
+ *
+ * <p>Example 1:
+ *
+ * <p>Input: nums = [2,7,11,15], target = 9 Output: [0,1] Explanation: Because nums[0] + nums[1] ==
+ * 9, we return [0, 1]. Example 2:
+ *
+ * <p>Input: nums = [3,2,4], target = 6 Output: [1,2] Example 3:
+ *
+ * <p>Input: nums = [3,3], target = 6 Output: [0,1]
+ */
+public class TwoSum1 {
+  public int[] twoSum(int[] nums, int target) {
+    for (int i = 0; i < nums.length; i++) {
+      var remainder = target - nums[i];
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[j] == remainder) return new int[] {i, j};
+      }
+    }
+    return null;
+  }
+}
