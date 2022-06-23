72ae2a616463d67f1216f1a69fab36a38c5304de
Anffer Castillo <anffer.castillo@gmail.com>
2022-06-23 12:36:19 -0300

    More binary tree

--- leetcode/BinaryTreeInOrderTraversal94.java
+++ leetcode/BinaryTreeInOrderTraversal94.java
@@ -1,7 +1,5 @@
 package leetcode;
 
-import trees.Tree;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,18 +16,52 @@ public class BinaryTreeInOrderTraversal94 {
 
     var test = new BinaryTreeInOrderTraversal94();
     test.inorderTraversal(node1).forEach(o -> System.out.print(o + " "));
+    System.out.println();
+    test.preorderTraversal(node1).forEach(o -> System.out.print(o + " "));
+      System.out.println();
+      test.postorderTraversal(node1).forEach(o -> System.out.print(o + " "));
   }
 
+    private List<Integer> postorderTraversal(TreeNode root) {
+        var list = new ArrayList<Integer>();
+        dfsPostorder(root, list);
+        return list;
+    }
+
     public List<Integer> inorderTraversal(TreeNode root) {
-        dfs(root);
+        dfsInOrder(root);
         return values;
     }
 
-    public void dfs(TreeNode node){
+    public List<Integer> preorderTraversal(TreeNode root) {
+      var list = new ArrayList<Integer>();
+
+      dfsPreorder(root, list);
+
+      return list;
+    }
+
+    private void dfsPreorder(TreeNode node, ArrayList<Integer> list) {
         if(node != null) {
-            dfs(node.left);
+            list.add(node.val);
+            dfsPreorder(node.left, list);
+            dfsPreorder(node.right, list);
+        }
+    }
+
+    private void dfsPostorder(TreeNode node, ArrayList<Integer> list) {
+        if(node != null) {
+            dfsPostorder(node.left, list);
+            dfsPostorder(node.right, list);
+            list.add(node.val);
+        }
+    }
+
+    public void dfsInOrder(TreeNode node){
+        if(node != null) {
+            dfsInOrder(node.left);
             values.add(node.val);
-            dfs(node.right);
+            dfsInOrder(node.right);
         }
     }
 }
