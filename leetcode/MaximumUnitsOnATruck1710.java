137371bef56b8f5ff9a1cfb36df3fb80ac1fe1a0
Anffer Castillo <anffer.castillo@gmail.com>
2022-07-01 15:48:15 -0300

    Maximum Units on a Truck

---
+++ leetcode/MaximumUnitsOnATruck1710.java
@@ -0,0 +1,59 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/*
+You are assigned to put some amount of boxes onto one truck. You are given a 2D array boxTypes, where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]:
+
+numberOfBoxesi is the number of boxes of type i.
+numberOfUnitsPerBoxi is the number of units in each box of the type i.
+You are also given an integer truckSize, which is the maximum number of boxes that can be put on the truck. You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
+
+Return the maximum total number of units that can be put on the truck.
+
+
+
+Example 1:
+
+Input: boxTypes = [[1,3],[2,2],[3,1]], truckSize = 4
+Output: 8
+Explanation: There are:
+- 1 box of the first type that contains 3 units.
+- 2 boxes of the second type that contain 2 units each.
+- 3 boxes of the third type that contain 1 unit each.
+You can take all the boxes of the first and second types, and one box of the third type.
+The total number of units will be = (1 * 3) + (2 * 2) + (1 * 1) = 8.
+
+Example 2:
+
+Input: boxTypes = [[5,10],[2,5],[4,7],[3,9]], truckSize = 10
+Output: 91
+ */
+public class MaximumUnitsOnATruck1710 {
+
+  public static void main(String[] args) {
+    var test = new MaximumUnitsOnATruck1710();
+
+    System.out.println(
+    test.maximumUnits(new int[][] {{5,10},{2,5},{4,7},{3,9}}, 10));// 91
+    System.out.println(test.maximumUnits(new int[][] {{1, 3}, {2, 2}, {3, 1}}, 4)); // 91
+  }
+
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        Arrays.sort(boxTypes, (a,b) -> b[1] - a[1] );
+        var count = 0;
+
+        for (int i = 0; i < boxTypes.length && truckSize > 0; i++) {
+          var boxes = 0;
+            if(truckSize - boxTypes[i][0] >= 0) {
+                boxes = boxTypes[i][0];
+            } else {
+              boxes = truckSize;
+          }
+            truckSize -= boxes;
+            count += boxes * boxTypes[i][1];
+        }
+
+        return count;
+    }
+}
