b492bdc4378f41aeb91c276d4349b7d3f04f43a5
Anffer Castillo <anffer.castillo@gmail.com>
2022-12-20 21:08:54 +0100

    improved remove islands problem

--- graphs/remove-islands.js
+++ graphs/remove-islands.js
@@ -12,64 +12,66 @@
  *  Deberiamos eliminar el item en la posicion 3-1 porque es un item sin conexion a los bordes.
  */
 
-const edges = [];
-const visited = new Set();
+const removeIslands = (land) => {
+  const directions = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0],
+  ];
+  let direction = 0;
+  const iterations = land.length * 2 + (land[0].length - 2) * 2;
 
-const goAroundEdges = (grid) => {
-  const columns = grid[0].length - 1;
-  const rows = grid.length - 1;
+  const noIslandsGrid = Array(land.length)
+    .fill()
+    .map(() => Array(land[0].length).fill(0));
+
   let row = 0;
   let col = 0;
-  let next = "";
-  let direction = "right";
 
-  while (next != `0-0`) {
-    if (grid[row][col] == 1) {
-      explore(grid, row, col, visited);
+  for (let i = 0; i <= iterations; i++) {
+    if (land[row][col] == 1) {
+      explore(land, row, col, noIslandsGrid);
     }
-    if (direction === "right") {
-      if (col < columns) col++;
-      else direction = "down";
-    } else if (direction == "down") {
-      if (row < rows) row++;
-      else direction = "left";
-    } else if (direction == "left") {
-      if (col > 0) col--;
-      else direction = "up";
-    } else if (direction == "up") {
-      if (row > 0) row--;
-      else direction = "right";
+
+    let step = directions[direction];
+
+    // check if we need a direction change
+    let rowInBound = row + step[0] < land.length && row + step[0] >= 0;
+    let colInBound = col + step[1] < land[0].length && col + step[1] >= 0;
+
+    if (!rowInBound || !colInBound) {
+      //get the right direction
+      direction = (direction + 1) % 4;
+      step = directions[direction];
     }
-    next = row + "-" + col;
+
+    row += step[0];
+    col += step[1];
   }
 
-  for (let k = 0; k < grid.length; k++) {
-    for (let l = 0; l < grid[k].length; l++) {
-      if (!visited.has(`${k}-${l}`) && grid[k][l] != 0) {
-        edges.push(`${k}-${l}`);
-      }
-    }
-  }
+  return noIslandsGrid;
 };
 
-const explore = (grid, row, col, visited) => {
+const explore = (grid, row, col, noIslandsGrid) => {
   const rowInbound = 0 <= row && row < grid.length;
   const colInbound = 0 <= col && col < grid[0].length;
 
-  if (!rowInbound || !colInbound) return false;
+  if (!rowInbound || !colInbound) return;
 
-  if (grid[row][col] == 0) return false;
+  if (grid[row][col] == 0) return;
 
-  if (visited.has(`${row}-${col}`)) return false;
-  visited.add(`${row}-${col}`);
+  if (noIslandsGrid[row][col] == 1) return;
 
-  explore(grid, row, col + 1, visited);
-  explore(grid, row - 1, col, visited);
-  explore(grid, row + 1, col, visited);
-  explore(grid, row, col - 1, visited);
+  noIslandsGrid[row][col] = 1;
+
+  explore(grid, row, col + 1, noIslandsGrid); //right
+  explore(grid, row + 1, col, noIslandsGrid); //down
+  explore(grid, row, col - 1, noIslandsGrid); //left
+  explore(grid, row - 1, col, noIslandsGrid); //up
 };
 
-/* const m = [
+const m = [
   [0, 0, 0, 0, 0, 0],
   [1, 0, 0, 1, 1, 0],
   [1, 1, 0, 0, 0, 0],
@@ -77,7 +79,7 @@ const explore = (grid, row, col, visited) => {
   [0, 1, 1, 1, 1, 0],
   [1, 1, 1, 0, 1, 0],
 ];
- */
+
 /* const m = [
   [1, 1, 1, 0, 1, 0],
   [0, 0, 1, 1, 0, 1],
@@ -87,14 +89,16 @@ const explore = (grid, row, col, visited) => {
   [1, 0, 0, 1, 0, 0],
 ]; */
 
-const m = [
-  [0, 0, 0, 1, 0],
-  [0, 0, 1, 1, 0],
-  [0, 0, 1, 1, 1],
-  [0, 1, 0, 0, 1],
-  [1, 0, 0, 1, 1],
-  [1, 0, 0, 1, 0],
-];
 
-goAroundEdges(m);
-console.log(edges);
+// const m = [
+//   [0, 0, 0, 1, 0],
+//   [0, 0, 1, 1, 0],
+//   [0, 0, 1, 1, 1],
+//   [0, 1, 0, 0, 1],
+//   [1, 0, 0, 1, 1],
+//   [1, 0, 0, 1, 0],
+// ];
+
+const result = removeIslands(m);
+
+result.forEach((arr) => console.log(`[${arr.join(',')}],`));
